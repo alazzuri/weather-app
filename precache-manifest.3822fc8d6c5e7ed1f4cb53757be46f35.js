39e5be09d578d1702bc45b0402c5d34c22ca5a78
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-05-20 20:04:17 -0300

    Updates

---
+++ precache-manifest.3822fc8d6c5e7ed1f4cb53757be46f35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d916dfce366bc26f7458e28b1183be",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6541b47c36915aeb38b2",
+    "url": "/weather-app/static/css/2.ca7b0c1f.chunk.css"
+  },
+  {
+    "revision": "7abff9888d5c51bccb81",
+    "url": "/weather-app/static/css/main.c7dbce5e.chunk.css"
+  },
+  {
+    "revision": "6541b47c36915aeb38b2",
+    "url": "/weather-app/static/js/2.9254f449.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.9254f449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7abff9888d5c51bccb81",
+    "url": "/weather-app/static/js/main.5ac90c09.chunk.js"
+  },
+  {
+    "revision": "55676f7ecad5f9b6942e",
+    "url": "/weather-app/static/js/runtime-main.58bc7688.js"
+  },
+  {
+    "revision": "b618e8cb78b2da66441c365167ceea79",
+    "url": "/weather-app/static/media/logo.b618e8cb.png"
+  }
+]);
