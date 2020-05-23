d77dfc9e14b5fbf019f2dbd339e46f08f5b30b83
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-05-23 13:29:44 -0300

    Refactoring forecast skeleton

--- src/components/ContentLoaders/ForecastLoader.js
+++ src/components/ContentLoaders/ForecastLoader.js
@@ -1,51 +1,49 @@
+//REACT
 import React from "react";
+
+//MATERIAL UI
 import Skeleton from "@material-ui/lab/Skeleton";
 import Divider from "@material-ui/core/Divider";
+
+//STYLES
+import "./styles.css";
 
 const ForecastLoader = () => {
   return (
     <div className="forecast">
-      <Skeleton variant="rect" className="card-body" height={"100%"}>
-        <Skeleton variant="rect" className="mb-2" height={210}>
-          <Skeleton className="w-50 mx-auto" height={30} />
+      <Skeleton variant="rect" height={"100%"}>
+        <Skeleton variant="rect" height={210}>
+          <Skeleton className="content-loader centered" height={30} />
           <Skeleton
             variant="circle"
             width={60}
             height={60}
-            className="mx-auto mb-2"
+            className="centered"
           ></Skeleton>
           <Skeleton
             variant="rect"
             width={60}
             height={60}
-            className="mx-auto"
+            className="centered"
           ></Skeleton>
-          <Skeleton
-            variant="text"
-            height={30}
-            className="mx-auto w-75"
-          ></Skeleton>
+          <Skeleton variant="text" height={30} className="centered"></Skeleton>
         </Skeleton>
         <Divider />
-        <Skeleton variant="rect" className="mb-2" height={210}>
-          <Skeleton className="w-50 mx-auto" height={30} />
+        <Skeleton variant="rect" height={210}>
+          <Skeleton className="content-loader centered" height={30} />
           <Skeleton
             variant="circle"
             width={60}
             height={60}
-            className="mx-auto mb-2"
+            className="centered"
           ></Skeleton>
           <Skeleton
             variant="rect"
             width={60}
             height={60}
-            className="mx-auto"
+            className="centered"
           ></Skeleton>
-          <Skeleton
-            variant="text"
-            height={30}
-            className="mx-auto w-75"
-          ></Skeleton>
+          <Skeleton variant="text" height={30} className="centered"></Skeleton>
         </Skeleton>
         <Divider />{" "}
       </Skeleton>
