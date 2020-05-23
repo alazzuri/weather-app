20c2a95fbe16d9fd5d7d13e678dfd26f06cb73c1
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-05-23 13:54:37 -0300

    refactoring forecastItem component

--- src/components/ForecastItem/index.js
+++ src/components/ForecastItem/index.js
@@ -1,15 +1,23 @@
+//REACT
 import React from "react";
+
+//LIBS
 import PropTypes from "prop-types";
-import WeatherData from "../WeatherData";
 import Divider from "@material-ui/core/Divider";
+
+//COMPONENTS
+import WeatherData from "../WeatherData";
+
+//STYLES
+import "./styles.css";
 
 const ForecastItem = ({ weekDay, hour, data }) => {
   return (
-    <div className="pt-4">
+    <div className="container">
       <h2>
         {weekDay.toUpperCase()} - {hour} hs.
       </h2>
-      <WeatherData data={data} className="mb-4" />
+      <WeatherData data={data} />
       <Divider />
     </div>
   );
